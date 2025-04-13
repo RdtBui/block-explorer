1d24d5227c03a3585e4276746d89e1d660a7956e
Richard Bui <43472079+RdtBui@users.noreply.github.com>
2025-04-13 18:23:00 -0400

    Add validations to Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,12 @@
 class Transaction < ApplicationRecord
+    # All columns from the Transaction is expected to be present under normal circumstances
+    validates :time, presence: true
+    validates :height, presence: true
+    validates :tx_hash, presence: true, uniqueness: true # Ensure no duplicate tx_hashes
+    validates :block_hash, presence: true, uniqueness: true # Ensure no duplicate block_hashes
+    validates :sender, presence: true
+    validates :receiver, presence: true
+    validates :gas_burnt, presence: true
+    validates :deposit, presence: true
+    validates :success, prepend: true
 end
